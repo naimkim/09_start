bdbbff79950b97dec1bf8b0d1e73b4989879cebf
naimkim <naimkim2000@naver.com>
2020-11-02 20:47:23 +0900

    02_average

--- main.c
+++ main.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define SIZE 5
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	int i;
-	int	grade[5];
+	int	average;
+	int sum = 0;
+	int grade[SIZE];
 	
-	grade[0] = 10;
-	grade[1] = 20;
-	grade[2] = 30;
-	grade[3] = 40;
-	grade[4] = 50;
+	printf("input the grade of 5 students. \n");
 	
-	for(i=0;i<5;i++)
-		printf("grade[%d] = %d\n", i, grade[i]);
+	for(i=0;i<SIZE;i++)
+		scanf("%d", &grade[i]);
+		
+	for(i=0;i<SIZE;i++)
+	{
+	printf("grade[%d] = %d\n", i, grade[i]);
+	sum += grade[i];
+	}
+	average = sum/SIZE;
+	printf("average of grade : %d\n", average);
+	
 	return 0;
 }
